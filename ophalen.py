bdd3bb1a516186f8af649723824139948d939ece
lexdorpmanns <lexdorpmanns@gmail.com>
2016-11-08 15:02:14 +0100

    ophalen

---
+++ ophalen.py
@@ -0,0 +1,15 @@
+import time
+import datetime
+
+def naamInvoeren():
+    #pakt de ingevoerde naam van de gebruiker
+    ingevoerdeNaam = input("Voer uw voor en achternaam in. ")
+    return ingevoerdeNaam
+
+def nummerCheck(nummer):
+    "kijkt of het nummer bestaat in fietsen.csv"
+    reader = csv.DictReader(open('fietsen.csv', 'r'))
+    dict_list = []
+    for line in reader:
+
+# check of de fiets bij de gebruiker hoort
