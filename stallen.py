e9b27e9c20d6a048808716d34bc00881da1af36c
thatdutchsir <thatdutchsir@gmail.com>
2016-11-08 16:04:11 +0100

    Csv dictreader edit

--- stallen.py
+++ stallen.py
@@ -1,33 +1,29 @@
 import time
 import datetime
 import csv
-def nummerInvoeren():
+def nummerCheck():
     #pakt het ingevoerde nummer van de gebruiker
     ingevoerdeNummer = (input("Voer uw gebruikersnummer in. ")
-    return ingevoerdeNummer
-def nummerCheck(nummer):
-    "kijkt of het nummer bestaat in gebruikers.csv"
+    #kijkt of het nummer bestaat in gebruikers.csv
     reader = csv.DictReader(open('gebruikers.csv', 'r'))
     dict_list = []
     for line in reader:
         dict_list.append(line)
     for dict in dict_list:
-        if nummer in dict['id']:
+        if ingevoerdeNummer in dict['id']:
             print('Uw ID ' + dict['id'] + ' is geaccepteerd.')
         else:
             print('Uw ID ' + dict['id'] + ' is niet correct. Controleer uw ID in uw e-mail.')
-def naamInvoeren():
+def naamCheck():
     #pakt de ingevoerde naam van de gebruiker
     ingevoerdeNaam = input("Voer uw voor en achternaam in. ")
-    return ingevoerdeNaam
-def naamCheck(naam):
     "Kijkt of de naam bestaat in gebruikers.csv"
     reader = csv.DictReader(open('gebruikers.csv', 'r'))
     dict_list = []
     for line in reader:
         dict_list.append(line)
     for dict in dict_list:
-        if naam in dict['naam']:
+        if ingevoerdeNaam in dict['naam']:
             print('Uw naam ' + dict['id'] + ' is geaccepteerd.')
         else:
             print('Uw naam ' + dict['id'] + ' is niet correct. Controleer uw naam in uw e-mail.')
@@ -35,7 +31,5 @@ def datumEnTijd():
     #geeft huidige datum en tijd
     huidigeDatumEnTijd = datetime.datetime.now().strftime('%d-%m-%Y, %H:%M:%S')
     return huidigeDatumEnTijd
-nummerInvoeren()
-nummerCheck(nummerInvoeren)
-naamInvoeren()
-naamCheck(naamInvoeren)
+nummerCheck()
+naamCheck()
