877f0d631d0816318627932d32e94b62ed24c0f8
thatdutchsir <thatdutchsir@gmail.com>
2016-11-08 12:44:28 +0100

    Csv dictreader edit

--- stallen.py
+++ stallen.py
@@ -11,12 +11,29 @@ def nummerCheck(nummer):
     reader = csv.DictReader(open('gebruikers.csv', 'r'))
     dict_list = []
     for line in reader:
-         
+        dict_list.append(line)
+    for dict in dict_list:
+        if nummer in dict['id']:
+            print('Uw ID ' + dict['id'] + ' is geaccepteerd.')
+        else:
+            print('Uw ID ' + dict['id'] + ' is niet correct. Controleer uw ID in uw e-mail.')
 
 def naamInvoeren():
     #pakt de ingevoerde naam van de gebruiker
     ingevoerdeNaam = input("Voer uw voor en achternaam in. ")
     return ingevoerdeNaam
+
+def naamCheck(naam):
+    "Kijkt of de naam bestaat in gebruikers.csv"
+    reader = csv.DictReader(open('gebruikers.csv', 'r'))
+    dict_list = []
+    for line in reader:
+        dict_list.append(line)
+    for dict in dict_list:
+        if naam in dict['naam']:
+            print('Uw naam ' + dict['id'] + ' is geaccepteerd.')
+        else:
+            print('Uw naam ' + dict['id'] + ' is niet correct. Controleer uw naam in uw e-mail.')
 
 def datumEnTijd():
     #geeft huidige datum en tijd
