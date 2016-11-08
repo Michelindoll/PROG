38ba3fc2348d5727d8bce553c426e30ca36dd47f
thatdutchsir <thatdutchsir@gmail.com>
2016-11-08 11:07:24 +0100

    Nummer input, naam input

--- stallen.py
+++ stallen.py
@@ -1,7 +1,18 @@
 import time
 import datetime
-def datumentijd():
+
+def nummerInvoeren():
+    #pakt het ingevoerde nummer van de gebruiker
+    ingevoerdeNummer = int(input("Voer uw gebruikersnummer in. "))
+    return ingevoerdeNummer
+
+def naamInvoeren():
+    #pakt de ingevoerde naam van de gebruiker
+    ingevoerdeNaam = input("Voer uw voor en achternaam in. ")
+    return ingevoerdeNaam
+
+def datumEnTijd():
     #geeft huidige datum en tijd
-    huidigedatumentijd = datetime.datetime.now().strftime('%d-%m-%Y, %H:%M:%S')
-    return huidigedatumentijd
-print(datumentijd())
+    huidigeDatumEnTijd = datetime.datetime.now().strftime('%d-%m-%Y, %H:%M:%S')
+    return huidigeDatumEnTijd
+
