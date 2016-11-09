29cba61ebb6634e10cc7c8539cd2c8e2903aa977
thatdutchsir <thatdutchsir@gmail.com>
2016-11-09 12:46:40 +0100

    Csv dictreader edit

--- stallen.py
+++ stallen.py
@@ -1,21 +1,21 @@
 import time
-import datetime
 import csv
 def nummerCheck():
     #pakt het ingevoerde nummer van de gebruiker
     ingevoerdeNummer = (input("Voer uw gebruikersnummer in. ")
     #kijkt of het nummer bestaat in gebruikers.csv
     reader = csv.DictReader(open('gebruikers.csv', 'r'))
-    dict_list = []
+    # Bovenstaande is een invalid syntax error?
+    dictList = []
     for line in reader:
-        dict_list.append(line)
-    for dict in dict_list:
+        dictList.append(line)
+    for dict in dictList:
         if ingevoerdeNummer in dict['id']:
             print('Uw ID ' + dict['id'] + ' is geaccepteerd.')
         else:
             print('Uw ID ' + dict['id'] + ' is niet correct. Controleer uw ID in uw e-mail.')
 def naamCheck():
-    #pakt de ingevoerde naam van de gebruiker
+    pakt de ingevoerde naam van de gebruiker
     ingevoerdeNaam = input("Voer uw voor en achternaam in. ")
     "Kijkt of de naam bestaat in gebruikers.csv"
     reader = csv.DictReader(open('gebruikers.csv', 'r'))
@@ -29,7 +29,8 @@ def naamCheck():
             print('Uw naam ' + dict['id'] + ' is niet correct. Controleer uw naam in uw e-mail.')
 def datumEnTijd():
     #geeft huidige datum en tijd
-    huidigeDatumEnTijd = datetime.datetime.now().strftime('%d-%m-%Y, %H:%M:%S')
+    huidigeDatumEnTijd = int(time.time())
     return huidigeDatumEnTijd
+print(datumEnTijd())
 nummerCheck()
 naamCheck()
