98aaef5e61659ed1de5f84e44628427137734592
FearsomePebble <marijn.van.oostveen@gmail.com>
2016-11-09 11:47:34 +0100

    registreren

--- Registreren.py
+++ Registreren.py
@@ -7,5 +7,23 @@ def id_Generator(size=6, chars=string.ascii_uppercase + string.digits):
 
 print(id_Generator())
 
+def registreren():
 
+    while True:
+        print('Welkom Bij Goat Sec fiets stallingen!')
+        print('Om te registreren bij Goat Sec hebben wij enkele persoonlijke gegevens nodig.')
+        print('Uw persoonlijke gegevens worden natuurlijk veilig opgeslagen.')
+        print('Uw persoonlijke gegevens worden natuurlijk niet misbruikt door Goat Sec Inc.')
+
+        gebruikersGegevens = open('tempgebruikers.csv', 'w')
+        nameInput = input('Voer uw voor en achternaam in: ')
+        emailInput = input('Voer uw Email in: ')
+        bankPinNummerinput = input('PIN: ')
+        fietsModelInput = input('Voer uw fiets model in: ')
+        gebruikersGegevens.write(nameInput + '\n')
+        gebruikersGegevens.write(emailInput + '\n')
+        gebruikersGegevens.write(bankPinNummerinput + '\n')
+        gebruikersGegevens.write(fietsModelInput + '\n')
+        break
+registreren()
 
