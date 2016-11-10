3a33e1558b0da72942230250ebea951c488be2b0
davey <davey_groen@hotmail.com>
2016-11-10 11:36:38 +0100

    Email functie

--- mail.py
+++ mail.py
@@ -1,29 +1,29 @@
 import smtplib
 
-def mail(naar, van, wachtwoord):
+def mail(naar):
 
-    #Naar en van wie je de email stuurt
+    #Naar en van wie je de email stuurt.
     to = naar
-    live_user = van
-    live_pwd = wachtwoord
+    live_user = 'schoolhu2016@hotmail.com'
+    live_pwd = 'ProjectTeam2'
 
-    #Instellingen voor de verzendende email
+    #Instellingen voor de verzendende email.
     smtpserver = smtplib.SMTP("smtp.live.com",587)
     smtpserver.ehlo()
     smtpserver.starttls()
     smtpserver.ehlo() # extra characters to permit edit
     smtpserver.login(live_user, live_pwd)
 
-    #Zet de gegevens op de juiste plaats. De tekst die hij uiteindelijk mee verstuurd
+    #Zet de gegevens op de juiste plaats. De tekst die hij uiteindelijk mee verstuurd.
     header = 'To:' + to + '\n' + 'From: ' + live_user + '\n' + 'Subject:Test \n'
     print(header)
     msg = header + '\nDit is een test bericht van Project team 2! \n\n'
 
-    #verstuurd de mail en als het lukt, print done! uit. Het sluit daarna de server af
+    #verstuurd de mail en als het lukt, print done! uit. Het sluit daarna de server af.
     smtpserver.sendmail(live_user, to, msg)
     print('done!')
     smtpserver.quit()
 
-#Vraagt naar de nodige gegevens en stuurd vervolgens de standaard email
+#Vraagt naar de nodige gegevens en stuurd vervolgens de standaard email.
 print('Email naar, Email van u(moet een live/hotmail zijn), Uw email wachtwoord')
-mail(input('To: '), input('From: '), input('Password: '))
+mail(input('To: '))
