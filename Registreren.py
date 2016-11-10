e9e4e63862c01f99462afca978c754953e019090
FearsomePebble <marijn.van.oostveen@gmail.com>
2016-11-10 12:51:39 +0100

    registreren

--- Registreren.py
+++ Registreren.py
@@ -14,18 +14,18 @@ def registreren():
         print('Om te registreren bij Goat Sec hebben wij enkele persoonlijke gegevens nodig.')
         print('Uw persoonlijke gegevens worden natuurlijk veilig opgeslagen.')
         print('Uw persoonlijke gegevens worden natuurlijk niet misbruikt door Goat Sec Inc.')
-
-        gebruikersGegevens = open('tempgebruikers.csv', 'w')
-        nameInput = input('Voer uw voor en achternaam in: ')
-        emailInput = input('Voer uw Email in: ')
-        bankPinNummerinput = input('PIN: ')
-        fietsModelInput = input('Voer uw fiets model in: ')
-        gebruikersGegevens.write(nameInput + '\n')
-        gebruikersGegevens.write(emailInput + '\n')
-        gebruikersGegevens.write(bankPinNummerinput + '\n')
-        gebruikersGegevens.write(fietsModelInput + '\n')
-        gebruikersGegevens.write(id_Generator())
+        schrijfFile()
         break
+
+
+def schrijfFile(ID, naam, email, fietsModel):
+    with open('gebruikers.csv', 'a', newline='') as gebruikersGegevens:
+        writer = csv.writer(gebruikersGegevens)
+        writer.writerow((ID, naam, email, fietsModel))
+        ID = id_Generator()
+        naam = input('Voer uw voor en achternaam in: ')
+        email = input('Voer uw Email in: ')
+        fietsModel = input('Voer uw fiets model in: ')
 
 
 
