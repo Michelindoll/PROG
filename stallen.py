f21233b79cee68a64f0814569e1b87c139afdd8a
thatdutchsir <thatdutchsir@gmail.com>
2016-11-10 17:15:25 +0100

    I DID IT BOYS

--- stallen.py
+++ stallen.py
@@ -1,42 +1,27 @@
 import time
 import csv
-def nummerCheck():
+def nummerNaamCheck():
     #pakt het ingevoerde nummer van de gebruiker
     global ingevoerdeNummer
     ingevoerdeNummer = (input('Voer uw ID in. '))
+    global ingevoerdeNaam
+    ingevoerdeNaam = input("Voer uw voor en achternaam in. ")
     #kijkt of het nummer bestaat in gebruikers.csv
     reader = csv.DictReader(open('gebruikers.csv', 'r'))
     dictList = []
     for line in reader:
         dictList.append(line)
     for dict in dictList:
-        if ingevoerdeNummer in dict['id']:
+        if ingevoerdeNummer in dict['id'] and ingevoerdeNaam in dict['naam']:
             global counter
             counter +=1
-            print('Uw ID ' + ingevoerdeNummer + ' is geaccepteerd.')
-            naamCheck()
+            print('Uw ID ' + ingevoerdeNummer + ' en uw naam ' + ingevoerdeNaam + ' is geaccepteerd.')
+        if counter == 1:
+            schrijfFile(ingevoerdeNummer, datumEnTijd(), ingevoerdeNaam)
             break
-    print('ID niet gevonden, probeer het nog eens')
-    nummerCheck()
-def naamCheck():
-    #pakt de ingevoerde naam van de gebruiker
-    global ingevoerdeNaam
-    ingevoerdeNaam = input("Voer uw voor en achternaam in. ")
-    "Kijkt of de naam bestaat in gebruikers.csv"
-    reader = csv.DictReader(open('gebruikers.csv', 'r'))
-    dict_list = []
-    for line in reader:
-        dict_list.append(line)
-    for dict in dict_list:
-        if ingevoerdeNaam not in dict['naam']:
-            print('Uw naam ' + ingevoerdeNaam + ' is niet correct. Controleer uw naam in uw e-mail.')
-            naamCheck()
-        if ingevoerdeNaam in dict['naam']:
-            global counter
-            counter +=1
-            print('Uw naam ' + ingevoerdeNaam + ' is geaccepteerd.')
-            schrijfFile(ingevoerdeNummer, datumEnTijd(),ingevoerdeNaam)
-            break
+    if counter != 1:
+        print('Uw ID of uw Naam is niet correct.')
+        nummerNaamCheck()
 def datumEnTijd():
     #geeft huidige datum en tijd
     huidigeDatumEnTijd = int(time.time())
@@ -44,12 +29,8 @@ def datumEnTijd():
 def schrijfFile(nummer, datum, naam):
     with open('fietsen.csv', 'a', newline='') as fietsFile:
         global counter
-        if counter == 2:
+        if counter == 1:
             writer = csv.writer(fietsFile)
             writer.writerow((nummer, datum, naam))
-        if counter != 2:
-            print('Uw ID of uw Naam is incorrect.')
-            nummerCheck()
-
 counter = 0
-nummerCheck()
+nummerNaamCheck()
