5162da7553a16356de464c95718aab0fb49047c7
thatdutchsir <thatdutchsir@gmail.com>
2016-11-10 16:32:06 +0100

    IT IZ WORKIN

--- stallen.py
+++ stallen.py
@@ -10,14 +10,15 @@ def nummerCheck():
     for line in reader:
         dictList.append(line)
     for dict in dictList:
+        if ingevoerdeNummer not in dict['id']:
+            print('Uw ID ' + ingevoerdeNummer + ' is niet correct. Controleer uw ID in uw e-mail.')
+            nummerCheck()
         if ingevoerdeNummer in dict['id']:
             global counter
             counter +=1
             print('Uw ID ' + ingevoerdeNummer + ' is geaccepteerd.')
             naamCheck()
-        if ingevoerdeNummer not in dict['id']:
-            print('Uw ID ' + ingevoerdeNummer + ' is niet correct. Controleer uw ID in uw e-mail.')
-            nummerCheck()
+            break
 def naamCheck():
     #pakt de ingevoerde naam van de gebruiker
     global ingevoerdeNaam
@@ -28,14 +29,15 @@ def naamCheck():
     for line in reader:
         dict_list.append(line)
     for dict in dict_list:
+        if ingevoerdeNaam not in dict['naam']:
+            print('Uw naam ' + ingevoerdeNaam + ' is niet correct. Controleer uw naam in uw e-mail.')
+            naamCheck()
         if ingevoerdeNaam in dict['naam']:
             global counter
             counter +=1
             print('Uw naam ' + ingevoerdeNaam + ' is geaccepteerd.')
             schrijfFile(ingevoerdeNummer, datumEnTijd(),ingevoerdeNaam)
-        if ingevoerdeNaam not in dict['naam']:
-            print('Uw naam ' + ingevoerdeNaam + ' is niet correct. Controleer uw naam in uw e-mail.')
-            naamCheck()
+            break
 def datumEnTijd():
     #geeft huidige datum en tijd
     huidigeDatumEnTijd = int(time.time())
